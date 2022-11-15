504d41a3433607f5c0afccc848ae879d2b2a4640
Hrishikesh Kakkad <hrishikeshkakkad@lucidmotors.com>
2022-11-15 15:40:40 -0500

    Normal - new

--- basecode/graph.py
+++ basecode/graph.py
@@ -4,63 +4,55 @@ import json
 import numpy as np
 
 
-
 with open('results.json') as f:
-  data = json.load(f)
-  
+    data = json.load(f)
 
-# print(data)
+# x_axis = []
+# y_axis_train_acc = []
+# y_axis_validation_acc = []
+# y_axis_test_acc = []
 
-fig = plt.figure(figsize = (10, 5))
+# x_tick_list = []
 
-x_axis = []
-y_axis_train_acc = []
-y_axis_validation_acc = []
-y_axis_test_acc = []
-
-x_tick_list = []
-
+plot_data = {}
 
 for x in range(len(data)):
-  if(data[x]["regularization"] % 10 == 0):
-    x_tick_list.append(data[x]["regularization"])
-    x_axis.append(data[x]["regularization"])
-    y_axis_train_acc.append(float(data[x]["train_acc"]))
-    y_axis_validation_acc.append(float(data[x]["validation_acc"]))
-    y_axis_test_acc.append(float(data[x]["test_acc"]))
-  
-  
-barWidth = 1
+    if(data[x]["regularization"] % 10 == 0):
+        if data[x]["hidden"] not in plot_data.keys():
+            plot_data[data[x]["hidden"]] = [data[x]]
+        else:
+            plot_data[data[x]["hidden"]].append(data[x])
 
-
-br1 = np.arange(len(y_axis_train_acc))
-br2 = [x + barWidth for x in br1]
-br3 = [x + barWidth for x in br2]
- 
-# Make the plot
-plt.bar(br1, y_axis_train_acc, color ='r', width = barWidth,
-        edgecolor ='grey', label ='train_acc')
-plt.bar(br2, y_axis_validation_acc, color ='g', width = barWidth,
-        edgecolor ='grey', label ='validation_acc')
-plt.bar(br3, y_axis_test_acc, color ='b', width = barWidth,
-        edgecolor ='grey', label ='test_acc')
-
-
-plt.xlabel('Regularization', fontweight ='bold', fontsize = 15)
-plt.ylabel('Acc', fontweight ='bold', fontsize = 15)
-plt.xticks([r + barWidth for r in range(len(x_tick_list))],
+for key, value in plot_data.items():
+    x_axis = []
+    y_axis_train_acc = []
+    y_axis_validation_acc = []
+    y_axis_test_acc = []
+    for val in value:
+        x_axis.append(val["regularization"])
+        y_axis_train_acc.append(float(val["train_acc"]))
+        y_axis_validation_acc.append(float(val["validation_acc"]))
+        y_axis_test_acc.append(float(val["test_acc"]))
+        
+    barWidth = 1
+    
+    fig = plt.figure(figsize=(6, 5))    
+    plt.xlabel('Regularization', fontweight ='bold', fontsize = 15)
+    plt.ylabel('Acc', fontweight ='bold', fontsize = 15)
+    plt.xticks([r + barWidth for r in range(len(value))],
         x_axis)
+    plt.title("Regularization vs Acc")
+    n = len(x_axis)
+    width = 0.25
+    plt.bar(np.arange(len(y_axis_train_acc)), y_axis_train_acc, color='black',
+                width=width, edgecolor='black',
+                label='y_axis_train_acc')
 
-  
-plt.legend()
-plt.show()
-  
-  
-
-
-
-  
-# print(x_axis, "x_axis")
-# print(y_axis_train_acc, "y_axis_train_acc")
-# print(y_axis_validation_acc, "y_axis_validation_acc")
-# print(y_axis_test_acc, "y_axis_test_acc")
+    plt.bar(np.arange(len(y_axis_validation_acc)), y_axis_validation_acc, color='blue',
+                width=width, edgecolor='black',
+                label='y_axis_validation_acc')
+    plt.bar(np.arange(len(y_axis_test_acc)), y_axis_test_acc, color='red',
+                width=width, edgecolor='black',
+                label='y_axis_test_acc')
+    plt.legend()
+    plt.show()
